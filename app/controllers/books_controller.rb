8d7891e0f1e49286cabd9f3e3d9ee0fa55c493a4
Rob Mitchell <robmitchell@email.com>
2019-03-04 11:39:18 +0100

    Book new create and index working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,39 @@
 class BooksController < ApplicationController
 
-def index
-end
+  def index
+    @books = Book.all
+  end
 
-def show
-end
+  def show
+  end
 
-def new
-end
+  def new
+    @book = Book.new
+  end
 
-def create
-end
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+   #   book_user = UserBook.create(book: @book, user: current_user, have_or_want: @book.have_read)
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
 
-def edit
-end
+  def edit
+  end
 
-def update
-end
+  def update
+  end
 
-def destroy
-end
+  def destroy
+  end
 
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
 
 end
