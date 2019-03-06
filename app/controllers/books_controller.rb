765c8ca87db353a53c4a7db7360db22ef08867f5
Rob Mitchell <robmitchell@email.com>
2019-03-06 18:10:29 +0100

    google books working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,4 @@
 class BooksController < ApplicationController
-
   before_action :find_book, only: :show
 
   def index
@@ -16,12 +15,56 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      create_user_book
+
       redirect_to books_path
     else
       render :new
     end
   end
+
+  def create
+    @book = Book.new(book_params)
+
+    #search_terms varible made as GoogleBooks.search won't except variables
+    search_terms = "inauthor:#{@book.author}, intitle:#{@book.title}, subject: 'Fiction' "
+    title = @book.title
+    google_books_objects = GoogleBooks.search(search_terms)
+
+    #Google books tends to ingnore subject so this removes non fiction
+    google_books = []
+    google_books_objects.each do |book|
+      if book.categories == "Fiction"
+        google_books << book
+      end
+    end
+
+    if google_books.empty?
+      flash.notice = "Couldn't find you're book, please try again."
+      redirect_to new_book_path
+    else
+
+    first_book = google_books.first
+
+    first_book.image_link(:zoom => 6)
+    # title and autors changed from search terms to prevent duplication. ie having Tolkien and J R Tolkien being differant authors
+    @book.title = first_book.title
+    @book.author = first_book.authors
+    #Finf the book if it exists with the same title and author, if not creates it.
+    @book = Book.where(title: @book.title, author: @book.author).first_or_create do |book|
+      book.photo_url = first_book.image_link
+      # book.description = first_book.description
+      # book.page_count = first_book.page_count
+      # book.isbn = first_book.isbn
+      # book.created_by = current_user.id
+    end
+    if @book.save
+      create_user_book
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+end
 
   def edit
   end
