45a86c7b9cab0c74bd27418ce1bf696734834f09
Rob Mitchell <robmitchell@email.com>
2019-03-06 10:22:49 +0100

    new book for changed to simple form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,
+       :recoverable, :rememberable, :validatable
 
   has_many :user_books, dependent: :destroy
   has_many :books, through: :user_books
