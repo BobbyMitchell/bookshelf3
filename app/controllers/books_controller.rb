6537ba7eed82d35f6d7afa454f4009cf1eeaba10
Rob Mitchell <robmitchell@email.com>
2019-03-07 15:08:55 +0100

    Extra fields added to books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,6 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-
       redirect_to books_path
     else
       render :new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190306143835) do
+ActiveRecord::Schema.define(version: 20190307140023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(version: 20190306143835) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "genre"
+    t.text "description"
+    t.bigint "isbn"
+    t.integer "page_count"
   end
 
   create_table "user_books", force: :cascade do |t|
